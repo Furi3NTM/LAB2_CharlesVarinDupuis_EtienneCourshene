5bc30a7a322710bee019292b23ff1e3a868042f3
Furi3NTM <charles.varindupuis@hotmail.fr>
2023-03-10 12:09:22 -0500

    Modification niveau 2
    ajout Script player + UI niveau

---
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Attributs 
+    [SerializeField] private float _vitesse = 800;
+    private bool _estActif = true;                     //??
+    private Rigidbody _rb;
+
+
+    // Méthodes privées
+   private void Start()
+    { 
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (_estActif)
+        {
+         MouvementsJoueur();
+        }
+   
+    }
+    
+
+    private void MouvementsJoueur()
+    {
+       float positionX = Input.GetAxis("Horizontal");
+       float positionZ = Input.GetAxis("Vertical");
+       Vector3 direction = new Vector3(positionX, 0f, positionZ);
+       //transform.Translate(direction * Time.deltaTime * _vitesse);
+       _rb.velocity = direction * Time.fixedDeltaTime * _vitesse;
+    }
+
+    public void StopJoueur()
+    {
+        this.gameObject.SetActive(false);
+    }
+   
+}
